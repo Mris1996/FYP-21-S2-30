3b23ff8c69b484c10a8dad012210ba4d1693583a
Mohamed Risvanudeen <risv96@gmail.com>
2021-04-24 00:22:13 +0800

    g

--- Negotiations.php
+++ Negotiations.php
@@ -159,7 +159,7 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
-            $(document).ready(function () {});
+          //  $(document).ready(function () {});
         </script>
 	</body>
 
