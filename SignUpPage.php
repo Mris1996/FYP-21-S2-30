c5e78830bdd5b7969df7494108f959bfb2c8d378
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-12 01:27:46 +0800

    Update SignUpPage.php

--- SignUpPage.php
+++ SignUpPage.php
@@ -145,24 +145,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['SignUpButton'])){
 		}
 
 		if ( !preg_match("#[0-9]+#", $_POST["SignUpPassword"]) ) {
+			echo"asssdx";
 			$submit = False;
 			$SignUpPasswordError = "Password must contain 8 characters at least , with alphabets and numbers";
 		}
 
 		if ( !preg_match("#[a-z]+#", $_POST["SignUpPassword"]) ) {
+			echo"asd";
 			$submit = False;
 			$SignUpPasswordError = "Password must contain 8 characters at least , with alphabets and numbers";
 		}
 
-		if ( !preg_match("#[A-Z]+#", $_POST["SignUpPassword"]) ) {
-			$submit = False;
-			$SignUpPasswordError = "Password must contain 8 characters at least , with alphabets and numbers";
-		}
 
-		if ( !preg_match("/[\'^£$%&*()}{@#~?><>,|=_+!-]/", $_POST["SignUpPassword"]) ) {
-			$submit = False;
-			$SignUpPasswordError = "Password must contain 8 characters at least , with alphabets and numbers";
-		}
 		
 	}
 	
