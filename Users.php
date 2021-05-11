e69f881f5adae6dd366ab0ff8cc15b97aa44c708
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-11 23:27:21 +0800

    Update Users.php

--- Users.php
+++ Users.php
@@ -515,9 +515,10 @@ class StandardUser extends BaseUser
 						break;
 					  }
 				}
-				echo"asd";
+			
 		$sql = "INSERT INTO `contracts`(`ContractID`,`InitialOffer`,`NewOffer`,`DateRequired`, `BuyerUserID`, `SellerUserID`, `ProductID`) VALUES ('".$ContractID."','".$InitialOffer."','".$Offer."','".$DateRequired."','".$this->getUID()."','".$SellerID."','".$ProductID."')";
 		$result = $this->connect()->query($sql) or die($this->connect()->error); 
+			echo"asd";
 		return $ContractID;
 	}
 	public function ListOfContracts($ContractType){
