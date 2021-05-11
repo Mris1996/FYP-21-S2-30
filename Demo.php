dec68b8191ec75d49d4082bb04e2d6ce1f194820
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-12 00:29:27 +0800

    Update Demo.php

--- Demo.php
+++ Demo.php
@@ -1,38 +1,13 @@
-<?php require_once("NavBar.php");
-/*
-for($x = 1;$x<50;$x++){
-
-if($x<10){
-	
-$_SESSION['Object']->ListProduct('Product'.$x,'Home and Lifestyle','Sample Description',round(1000, 0),'Sample caption','/images/sample.jpg');
-echo $x;
-}
-if($x>=10&&$x<20){
-$_SESSION['Object']->ListProduct('Product'.$x,'Sports','Sample Description',round(1000, 0),'Sample caption','/images/sample.jpg');
-}
-if($x>=20&&$x<30){
-$_SESSION['Object']->ListProduct('Product'.$x,'Music','Sample Description',round(1000, 0),'Sample caption','/images/sample.jpg');
-}
-if($x>=30&&$x<40){
-$_SESSION['Object']->ListProduct('Product'.$x,'Clothing','Sample Description',round(1000, 0),'Sample caption','/images/sample.jpg');
-}
-if($x>=40&&$x<50){
-$_SESSION['Object']->ListProduct('Product'.$x,'Electronics','Sample Description',round(1000, 0),'Sample caption','/images/sample.jpg');
-}
-}
-*/
-
-
-
-for($x=0;$x<1;$x++){
-			$raw_data = file_get_contents('http://localhost:4000/getBalance');
-			if($raw_data==null){
-				$raw_data = file_get_contents('http://localhost:400'.$x.'/getBalance');
-				$x=$x+1;
-				echo $x;
-			}
-			
-		}
-
+<?php 
+$ContractID = "asdas";
+$InitialOffer = 1;	
+$Offer = 1;
+$DateRequired = '14/07/2021';
+$UID = 'asdasdsadas';
+$SellerID  = 'asdaasdsdsadas';
+$ProductID = 'adqa1e21312dasdqas';
+$sql = "INSERT INTO `contracts`(`ContractID`,`InitialOffer`) VALUES ('".$ContractID."','".$InitialOffer."','".$Offer."','".$DateRequired."','".$UID."','".$SellerID."','".$ProductID."')";
+		$result = $this->connect()->query($sql) or die($this->connect()->error); 
+echo "asd";
 
 ?>
