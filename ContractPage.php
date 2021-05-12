b70491d2a92e1ee775b7bf5c115d089c56cd18a4
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-12 16:53:11 +0800

    Update ContractPage.php

--- ContractPage.php
+++ ContractPage.php
@@ -633,7 +633,7 @@ function SendRefund_Admin(){
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-var connection = new WebSocket('ws://localhost:3030');
+var connection = new WebSocket('wss://localhost:3030');
 var connectingSpan = document.getElementById("connecting");
 connection.onopen = function () {
 	
