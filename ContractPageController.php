c2a39dcda2c1e9c06ee96ae73dc6a80e035f0687
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-13 09:37:56 +0800

    Update ContractPageController.php

--- ContractPageController.php
+++ ContractPageController.php
@@ -203,7 +203,7 @@ curl_setopt(CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
 // Just return the transfer
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // execute
 //$response = curl_exec($ch);
 	
