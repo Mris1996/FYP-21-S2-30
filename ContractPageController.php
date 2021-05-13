849be33f96b163a0a25dd4afbbf312b5c34c4319
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-13 11:54:02 +0800

    Update ContractPageController.php

--- ContractPageController.php
+++ ContractPageController.php
@@ -199,12 +199,13 @@ $query = http_build_query(['data' => $jsonData]);
 if(isset($query)){
 	
 curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-// Just return the transfer
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // execute
 $response = curl_exec($ch);
  // close
  $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
- echo $retcode;
+echo $response;
 curl_close($ch);
 }
