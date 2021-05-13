3c306d890e42fbf2234c7ffed413feacb44231c3
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-13 09:33:23 +0800

    Update ContractPageController.php

--- ContractPageController.php
+++ ContractPageController.php
@@ -215,7 +215,7 @@ $host    = "localhost";
 		$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 		$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
 		if($result) { 
-		socket_write($socket, $query, strlen($message)) or die("Could not send data to server\n");
+		socket_write($socket, $query, strlen($query)) or die("Could not send data to server\n");
 		$result = socket_read ($socket, 1024) or die("Could not read server response\n");
 		}
 		socket_close($socket);
