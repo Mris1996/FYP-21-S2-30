4ab10be31860e88542f61fe9df22d7653a8b821a
Mohamed Risvanudeen <risv96@gmail.com>
2021-05-18 00:07:27 +0800

    Update Users.php

--- Users.php
+++ Users.php
@@ -422,7 +422,7 @@ class BaseUser
 	}
 	public function connect(){
 		$servername= "localhost";
-		$username = "root";
+		$username = "pma";
 		$password = "";
 		$dbname = "sticdb";
 		$conn = new mysqli($servername, $username, $password, $dbname);
